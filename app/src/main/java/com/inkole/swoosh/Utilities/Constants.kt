1d5660aa1b08ec8fe5b128cfe0c30998e609ac3f
Ivan Kolev <vancyki@abv.bg>
2017-11-26 13:36:29 +0200

    add player class to pass data through activities

--- app/src/main/java/com/inkole/swoosh/Utilities/Constants.kt
+++ app/src/main/java/com/inkole/swoosh/Utilities/Constants.kt
@@ -4,5 +4,4 @@ package com.inkole.swoosh.Utilities
  * Created by Vankata on 11/26/2017.
  */
 
-const val EXTRA_LEAGUE = "league"
-const val EXTRAL_SKILL = "skill"
+const val EXTRA_PLAYER = "player"
